9eb465585766291171a9d8d8a4398e20a21f6998
yarbest <72338354+yarbest@users.noreply.github.com>
2020-10-25 22:26:22 +0200

    z

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+let toggleNavigation = document.querySelector(".header__toggle-navigation");
+let navigation = document.querySelector(".header__navigation");
+
+toggleNavigation.addEventListener("click", () => {
+    navigation.classList.toggle("visible-navigation");
+});
+
 let toggleSidebar = document.querySelector(".header__toggle-sidebar");
 let sidebar = document.querySelector(".sidebar");
 
